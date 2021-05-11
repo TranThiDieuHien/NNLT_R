28468d1a98316250162c79ef7de7301583dc2458
TranThiDieuHien <72079838+TranThiDieuHien@users.noreply.github.com>
2021-05-11 11:19:23 +0700

    Add files via upload

---
+++ BT_11_05_2021.R
@@ -0,0 +1,40 @@
+setwd("C:\\Users\\DUC-PC\\Downloads\\")
+O = read.table(file ="Owls.txt", header = TRUE, dec=".")
+names(O)
+str(O)
+
+#Cau 1
+AllNest <- unique(O$ï..Nest)
+N <- length(AllNest)
+
+for (i in 1:N){
+  Nest.i <- as.character(AllNest[i])
+  Oi <- O[O$ï..Nest == Nest.i,]?  name <- Nest.i
+  name <- paste(Nest.i,".jpg", sep = "")
+  #jpeg(file = AllNest[i])
+  plot(x=Oi$ArrivalTime, y = Oi$SiblingNegotiation,
+       xlab="Arrival Time", 
+       ylab="Sibling Negotiation",
+       main = Nest.i)
+  dev.off()
+}
+
+
+
+
+#Cau 2
+O$NestNi?ht <- paste(O$ï..Nest, O$FoodTreatment,sep = "_") 
+head(O)
+AllNestsNights <- unique(O$NestNight)
+N <- length(AllNestsNights)
+for (i in 1:N){
+  NestNight.i <- as.character(AllNestsNights[i])
+  Oi <- O[O$NestNight ==NestNight.i,]
+  name <- NestNight.i
+  name?<- paste(NestNight.i, ".jpg", sep="")
+  #jpeg(file = name)
+  plot(x=O$ArrivalTime, y=O$NegPerChick,
+       xlab ="Arrivale Time",
+       ylab = "Sibling Negotiation",
+       main = NestNight.i)
+}
